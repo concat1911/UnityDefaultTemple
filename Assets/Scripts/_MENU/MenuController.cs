90a0f5baf7669cf858a538f781611c601bba2823
Tran Nhat Linh <nhatlinhtr95@gmail.com>
2019-10-24 20:11:07 +0200

    update MainMenu

--- Assets/Scripts/_MENU/MenuController.cs
+++ Assets/Scripts/_MENU/MenuController.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour {
+	[Header("Settings")]
+	public string newgame = "NewGame";
+
+	public void StartGame(){
+		SceneManager.LoadScene(newgame);
+	}
+
+	public void OpenOptions(){
+
+	}
+
+	public void CloseOption(){
+
+	}
+
+	public void OpenCredits(){
+
+	}
+
+	public void CloseCredits(){
+
+	}
 
 	public void LoadScene(int sceneIndex){
 		SceneManager.LoadScene(sceneIndex);
